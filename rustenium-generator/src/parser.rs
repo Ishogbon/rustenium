d2f59619bebc1317d8659da4018ec6662ed6aa75
Daniel Shogbon <ishogbon@gmail.com>
2025-04-02 21:51:34 +0100

    added full type,command,event look for

--- rustenium-generator/src/parser.rs
+++ rustenium-generator/src/parser.rs
@@ -6,7 +6,9 @@ pub fn parse_bs_file(content: &str) {
     let re_module = Regex::new(r"##\s+(?:The\s+)?(\w+)\s+Module\s+##\s+\{#module-([^}]+)\}").unwrap();
 
     // New regex to find ALL type definitions under a module
-    let re_type = Regex::new(r"####\s+The\s+([^\s.]+)\.([^\s]+)\s+Type\s+####\s+\{#type-([^}]+)-([^}]+)\}").unwrap();
+    let re_type = Regex::new(r"####\s+The\s+([^\s.]+)\.([^\s]+)\s+Type\s+####\s+\{#(?:type|types)-([^}]+)-([^}]+)\}").unwrap();
+    let re_command = Regex::new(r"####\s+The\s+([^\s.]+)\.([^\s]+)\s+Command\s+####\s+\{#command-([^}]+)-([^}]+)\}").unwrap();
+    let re_event = Regex::new(r"####\s+The\s+([^\s.]+)\.([^\s]+)\s+Event\s+####\s+\{#event-([^}]+)-([^}]+)\}").unwrap();
 
     for caps in re_module.captures_iter(content) {
         let module_keyword = caps.get(1).unwrap().as_str();
@@ -19,6 +21,24 @@ pub fn parse_bs_file(content: &str) {
 
             if type_module == module_keyword {
                 println!("Found type: '{}.{}'", module_keyword, type_name);
+            }
+        }
+
+        for command_caps in re_command.captures_iter(content) {
+            let command_module = command_caps.get(1).unwrap().as_str();
+            let command_name = command_caps.get(2).unwrap().as_str();
+
+            if command_module == module_keyword {
+                println!("Found Command: '{}.{}'", module_keyword, command_name);
+            }
+        }
+
+        for event_caps in re_event.captures_iter(content) {
+            let event_module = event_caps.get(1).unwrap().as_str();
+            let event_name = event_caps.get(2).unwrap().as_str();
+
+            if event_module == module_keyword {
+                println!("Found Event: '{}.{}'", module_keyword, event_name);
             }
         }
     }
